b318c9b3f3fe41ac3cbc788856f12c56993cab4d
enenery <erii.sugimoto@stu.bmcc.cuny.edu>
2017-10-07 14:06:32 -0400

    clean Circle.java and Shape.java

--- hw6/q1/child/Circle.java
+++ hw6/q1/child/Circle.java
@@ -2,23 +2,12 @@ package child;
 import parent.Shape;
 
 public class Circle extends Shape{
-	public Circle(){
-		
-	}
-	
-	public Circle(double a){
-		area = a;
-	}
+	public Circle(){}
 	
 	public static void main(String args[]){
-		Circle c = new Circle(3.0);
 		printHasShape(); //print hasShape in Shape
 		setFalseHasShape(); //set hasShape in Shape to false
 		printHasShape(); //print hasShape in Shape
-	}
-	
-	public void printArea(){
-		System.out.println("area is " + area);
 	}
 
 	public static void setFalseHasShape(){
